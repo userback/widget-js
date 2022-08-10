2eb703e0ab5d8f91f81c87f9a3e649f4cc9e3e7b
Mark Beukers <mark@afrotoss.com>
2022-08-10 22:34:24 +1000

    lint examples

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -1,29 +1,31 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
+import React, { useState } from 'react';
 import { useUserback } from '@userback/react';
+import reactLogo from './assets/react.svg';
 
 function App() {
     const [count, setCount] = useState(0);
     // Get Userback hooks
-    const { show, hide, open } = useUserback()
+    const { show, hide, open } = useUserback();
 
     return (
         <div className="App">
             <div>
-                <a href="https://reactjs.org" target="_blank">
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+                <button type="button" onClick={() => setCount((c) => c + 1)}>
+                    <span>count is: </span>
+                    {count}
+                </button>
                 <hr />
-                <button onClick={() => open('bug')}>Open Bugs</button>
-                <button onClick={() => open('general', 'screenshot')}>Screenshot me!</button>
+                <button type="button" onClick={() => open('bug')}>Open Bugs</button>
+                <button type="button" onClick={() => open('general', 'screenshot')}>Screenshot me!</button>
                 <hr />
-                <button onClick={() => hide()}>Hide</button>
-                <button onClick={() => show()}>Show</button>
+                <button type="button" onClick={() => hide()}>Hide</button>
+                <button type="button" onClick={() => show()}>Show</button>
             </div>
         </div>
     );
--- examples/vue/src/main.ts
+++ examples/vue/src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import UserbackPlugin from '@userback/vue'
+import { createApp } from 'vue';
+import './style.css';
+import UserbackPlugin from '@userback/vue';
+import App from './App.vue';
 
 const token = import.meta.env?.VITE_UB_TOKEN;
 const domain = import.meta.env?.VITE_UB_DOMAIN;
 
 createApp(App)
     .use(UserbackPlugin, { token, domain })
-    .mount('#app')
+    .mount('#app');
