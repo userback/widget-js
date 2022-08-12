1e835e3254b67bef9c7f5c998025cbc2c485b028
Mark Beukers <mark@afrotoss.com>
2022-08-12 13:43:08 +1000

    Fix rollup warnings
    Set export strategy explicitly and define externals.

--- widget-react/rollup.config.js
+++ widget-react/rollup.config.js
@@ -3,9 +3,10 @@ import typescript from '@rollup/plugin-typescript';
 export default {
     plugins: [typescript()],
     input: 'react.tsx',
+    external: ['react', 'react-dom', '@userback/widget'],
     output: [
-        { file: './dist/react.js', format: 'cjs' },
-        { file: './dist/react.mjs', format: 'esm' },
-        { file: './dist/react.umd.js', format: 'umd', name: 'Userback' },
+        { file: './dist/react.js', format: 'cjs', exports: 'named' },
+        { file: './dist/react.mjs', format: 'esm', exports: 'named' },
+        { file: './dist/react.umd.js', format: 'umd', name: 'Userback', exports: 'named' },
     ],
 };
--- widget-vue/rollup.config.js
+++ widget-vue/rollup.config.js
@@ -3,9 +3,10 @@ import typescript from '@rollup/plugin-typescript';
 export default {
     plugins: [typescript()],
     input: 'vue.tsx',
+    external: ['vue', '@userback/widget'],
     output: [
-        { file: './dist/vue.js', format: 'cjs' },
-        { file: './dist/vue.mjs', format: 'esm' },
-        { file: './dist/vue.umd.js', format: 'umd', name: 'Userback' },
+        { file: './dist/vue.js', format: 'cjs', exports: 'default' },
+        { file: './dist/vue.mjs', format: 'esm', exports: 'default' },
+        { file: './dist/vue.umd.js', format: 'umd', name: 'Userback', exports: 'default' },
     ],
 };
