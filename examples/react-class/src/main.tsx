399bbc5880f6cfac22e70d4af85d53e6c85ddb9c
Mark Beukers <mark@afrotoss.com>
2022-08-16 18:46:55 +1000

    Fix all linting issues :tada:

--- examples/react-class/src/main.tsx
+++ examples/react-class/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { UserbackProvider } from '@userback/react';
-import App from './App';
+import App from './App'; // eslint-disable-line import/no-unresolved
 import './index.css';
 
 // Get token from `../../.env`
