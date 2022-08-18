afb5a727eafe921cffc8ede50424c82eaad28aa6
Mark Beukers <mark@afrotoss.com>
2022-08-18 15:08:09 +1000

    fix e2e again

--- e2e/tests/userback.spec.ts
+++ e2e/tests/userback.spec.ts
@@ -25,7 +25,8 @@ test('Userback React Widget loads and can be opened', async ({ page }) => {
     await page.goto('http://example-react:3000/');
 
     // Authide is on
-    page.locator('text="Hide"').click();
+    await page.locator('text="Show"').click();
+    await page.waitForSelector('.userback-button.userback-button-e');
 
     // Open the userback widget
     const widget = page.locator('.userback-button.userback-button-e');
