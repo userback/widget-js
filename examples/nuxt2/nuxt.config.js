3b03db427cca2eb67a186e561931f8e32c433712
Mark Beukers <mark@afrotoss.com>
2022-10-10 15:10:37 +1000

    Add Nuxt2 example

---
+++ examples/nuxt2/nuxt.config.js
@@ -0,0 +1,54 @@
+import * as dotenv from 'dotenv';
+dotenv.config({ path: '../../.env' });
+
+export default {
+    // Target: https://go.nuxtjs.dev/config-target
+    target: 'static',
+    head: {
+        title: 'Userback nuxt example',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        ],
+    },
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+        '~/plugins/userback.client.js',
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        '@nuxt/typescript-build',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+//        '@userback/vue',
+    ],
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+    },
+    alias: {
+        // @FIXME: Appears to be an issue with .mjs not being loaded correctly
+        '@userback/vue': '@userback/vue/dist/vue.js',
+    },
+    env: {
+        UB_TOKEN: process.env?.VITE_UB_TOKEN,
+        UB_DOMAIN: process.env?.VITE_UB_DOMAIN,
+    },
+};
---
+++ examples/nuxt2/plugins/userback.client.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import UserbackPlugin from '@userback/vue';
+// import { defineNuxtPlugin } from '@nuxtjs/composition-api';
+
+const token = process.env.UB_TOKEN;
+const domain = process.env.UB_DOMAIN;
+
+Vue.use(UserbackPlugin, { token, domain });
+//export default defineNuxtPlugin((nuxtApp) => {
+//    nuxtApp.vueApp.use(UserbackPlugin, { token, domain })
+//})
