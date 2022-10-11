02a82b511506ee4342e6ce1c76b46fe8152bded5
Mark Beukers <mark@afrotoss.com>
2022-10-11 18:17:41 +1000

    lintfix

--- examples/nuxt/plugins/userback.client.js
+++ examples/nuxt/plugins/userback.client.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-undef */
 import UserbackPlugin from '@userback/vue';
 
 export default defineNuxtPlugin((nuxtApp) => {
     const { UB_TOKEN: token, UB_DOMAIN: domain } = useRuntimeConfig();
     nuxtApp.vueApp.use(UserbackPlugin, { token, domain });
-})
+});
