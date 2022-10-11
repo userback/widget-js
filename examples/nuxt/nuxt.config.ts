7cc831da8ac48f81b2b9b9144e64bac9f4721000
Mark Beukers <mark@afrotoss.com>
2022-10-11 18:17:41 +1000

    lintfix

--- examples/nuxt/nuxt.config.ts
+++ examples/nuxt/nuxt.config.ts
@@ -1,13 +1,15 @@
 import * as dotenv from 'dotenv';
+
 dotenv.config({ path: '../../.env' });
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+// eslint-disable-next-line no-undef
 export default defineNuxtConfig({
     target: 'static',
     runtimeConfig: {
         public: {
             UB_TOKEN: process.env?.VITE_UB_TOKEN,
             UB_DOMAIN: process.env?.VITE_UB_DOMAIN,
-        }
-    }
+        },
+    },
 });
