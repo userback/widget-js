6eb5dd686b5b4880baa87ac0f0b95818b522b1b7
Mark Beukers <mark@afrotoss.com>
2023-01-12 16:12:29 +1000

    Add Nuxt3 Support (#43)
    * Add Nuxt3 Example
    
    Create a new example using the nuxt3 starter template that loads the
    userback vue module using a custom plugin.
    
    * update playwright/typescript
    * skip webkit for nuxt3
    * update docs

--- e2e/tests/userback.spec.ts
+++ e2e/tests/userback.spec.ts
@@ -25,11 +25,6 @@ test('Userback React Widget loads and can be opened', async ({ page }) => {
 
     await page.goto('http://example-react:3000/');
 
-    // When Authide is on
-    // await page.waitForSelector(FEEDBACK_BTN, { state: 'hidden' });
-    // await page.locator('text="Show"').click();
-    // await page.waitForSelector(FEEDBACK_BTN);
-
     // Open the userback widget
     await (await page.waitForSelector(FEEDBACK_BTN)).click();
 
@@ -74,6 +69,23 @@ test('Userback Nuxt2 Widget loads and can be opened', async ({ page }) => {
     page.on('console', (message) => { console.log(message); });
 
     await page.goto('http://example-nuxt2:3000/');
+
+    // Open the userback widget
+    await (await page.waitForSelector(FEEDBACK_BTN)).click();
+
+    // Check for pop-up content
+    await expect(page.locator('img.userback-controls-logo')).toHaveCount(1);
+    await expect(page.locator('text="Report a bug"')).toHaveCount(1);
+});
+
+test('Userback Nuxt3 Widget loads and can be opened', async ({ page, browserName }) => {
+    test.skip(browserName === 'webkit', 'An error causing safari not to load the userback widget??'); // @FIXME
+
+    // Error Handling
+    page.on('pageerror', (err) => { console.error(err.message); });
+    page.on('console', (message) => { console.log(message); });
+
+    await page.goto('http://example-nuxt:3000/');
 
     // Open the userback widget
     await (await page.waitForSelector(FEEDBACK_BTN)).click();
