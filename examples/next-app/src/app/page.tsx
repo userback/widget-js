959f83db3270dea2c987a3827a0839538c546f39
Adrian Liu <aliu@userback.io>
2025-11-18 11:25:18 +1000

    Fix lint warning

--- examples/next-app/src/app/page.tsx
+++ examples/next-app/src/app/page.tsx
@@ -8,14 +8,7 @@ export default function Home() {
     return (
         <div className={styles.page}>
             <main className={styles.main}>
-                <Image
-                  className={styles.logo}
-                  src="/next.svg"
-                  alt="Next.js logo"
-                  width={180}
-                  height={38}
-                  priority
-                />
+                <Image className={styles.logo} src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
                 <Main />
             </main>
         </div>
